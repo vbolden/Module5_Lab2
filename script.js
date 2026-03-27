5fdf88244dd71541a037f997e9a68e22f871d301
Valerie Bolden <valerie.bolden@gmail.com>
2026-03-27 19:11:51 -0400

    added event listeners with validation for email and password inputs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // VALUES
 let form = document.getElementById('registrationForm');
 let username = document.getElementById('username');
-let nameErrorMsg = document.getElementById('usernameError');
+let usernameErrorMsg = document.getElementById('usernameError');
 let email = document.getElementById('email');
 let emailErrorMsg = document.getElementById('emailError');
 let password = document.getElementById('password');
@@ -22,3 +22,44 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     sessionStorage.setItem('username', username.value);
 });
+
+username.addEventListener('blur', (e) => {
+    if(username.value == ''){
+        username.setCustomValidity('Please enter your username')
+    } else {
+        username.setCustomValidity('')
+    }
+    username.reportValidity()
+    
+})
+
+email.addEventListener('blur', (e) => {
+    if(email.value == '') {
+        email.setCustomValidity('Please enter your email')
+    } else {
+        email.setCustomValidity('')
+    }
+    email.reportValidity()
+})
+
+password.addEventListener('blur', (e) => {
+    if(password.value == '') {
+        password.setCustomValidity('Please enter your password')
+        password.reportValidity()
+        return
+    } else if(password.validity.patternMismatch){
+        password.setCustomValidity('')
+    }else {
+        email.setCustomValidity('')
+    }
+})
+
+confirmPassword.addEventListener('blur', (e) => {
+    if(confirmPassword.value !== password.value) {
+        confirmPassword.setCustomValidity("Passwords do not match");
+        confirmPassword.reportValidity()
+        confirmPassword.style.borderColor = 'red';
+    } else {
+        confirmErrorMsg.innerHTML = "";
+    }
+})
