2eae22d7b56a68b2bd05359baf315f5bde53b946
Valerie Bolden <valerie.bolden@gmail.com>
2026-03-27 11:25:22 -0400

    cached values from HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+// VALUES
+let form = document.getElementById('registrationForm');
+let userName = document.getElementById('username');
+let nameErrorMsg = document.getElementById('usernameError');
+let email = document.getElementById('email');
+let emailErrorMsg = document.getElementById('emailError');
+let password = document.getElementById('password');
+let passwordErrorMsg = document.getElementById('passwordError');
+let confirmPassword = document.getElementById('confirmPassword');
+let confirmErrorMsg = document.getElementById('confirmPasswordError');
